f7d6e9bc51fc438b5a8e9769161ee303580817a4
holanv <20854147+holaneu@users.noreply.github.com>
2024-09-20 14:00:32 +0200

    Add files via upload

---
+++ quizzes-data.js
@@ -0,0 +1,116 @@
+const jsFileQuizzesData = [
+  {
+    "name": "Živá a neživá příroda",
+    "created": "2024-09-19",
+    "tags": ["přírodověda"],
+    "questions": [
+      {
+        "question": "Co je příkladem živé přírody?",
+        "options": ["Skála", "Strom", "Kámen", "Voda"],
+        "answer": 1,
+        "hint": "Živá příroda zahrnuje organismy, které rostou a vyvíjejí se."
+      },
+      {
+        "question": "Co nepatří mezi živou přírodu?",
+        "options": ["Kytka", "Pes", "Auto", "Hmyz"],
+        "answer": 2,
+        "hint": "Neživá příroda zahrnuje objekty, které nejsou organismy."
+      },
+      {
+        "question": "Který z následujících příkladů je součástí neživé přírody?",
+        "options": ["Pták", "Kámen", "Kočka", "Strom"],
+        "answer": 1,
+        "hint": "Neživá příroda zahrnuje neživé objekty a materiály."
+      },
+      {
+        "question": "Který organismus patří mezi živou přírodu?",
+        "options": ["Květina", "Mrak", "Moře", "Vítr"],
+        "answer": 0,
+        "hint": "Živá příroda zahrnuje organismy, jako jsou rostliny a zvířata."
+      },
+      {
+        "question": "Co patří mezi neživou přírodu?",
+        "options": ["Hora", "Slon", "Tráva", "Včela"],
+        "answer": 0,
+        "hint": "Neživá příroda jsou přírodní objekty, které nejsou živými organismy."
+      }
+    ]
+  },
+  
+  {
+    "name": "Emoce z filmu V hlavě",
+    "created": "2024-09-19",
+    "tags": ["emoce"],
+    "questions": [
+      {
+        "question": "Která z emocí ve filmu \"V hlavě\" je zodpovědná za regulaci strachu?",
+        "options": ["Radost", "Strach", "Smutek", "Znechucení"],
+        "answer": 1,
+        "hint": "Tato emoce nás udržuje ve střehu před nebezpečím."
+      },
+      {
+        "question": "Jaká emoce v \"V hlavě\" nejčastěji pomáhá při vytváření pozitivních vzpomínek?",
+        "options": ["Hněv", "Strach", "Radost", "Znechucení"],
+        "answer": 2,
+        "hint": "Tato emoce je spojená s radostí a veselím."
+      },
+      {
+        "question": "Která z emocí ve filmu má tendenci být pesimistická a připravovat na horší scénáře?",
+        "options": ["Smutek", "Radost", "Znechucení", "Strach"],
+        "answer": 0,
+        "hint": "Její roli je často nepochopená, ale důležitá pro emocionální rovnováhu."
+      },
+      {
+        "question": "Která emoce v \"V hlavě\" se zaměřuje na ochranu před fyzickým nebezpečím a infekcí?",
+        "options": ["Hněv", "Strach", "Znechucení", "Radost"],
+        "answer": 2,
+        "hint": "Tato emoce nás chrání před věcmi, které mohou být škodlivé."
+      }
+    ]
+  },
+
+  {
+    "name": "quiz 1",
+    "created": 1725959880633,
+    "tags": ["tag1"],
+    "questions": [
+      {
+        "question": "Question 1",
+        "options": ["option 1", "option 2", "option 3", "option 4"],
+        "answer": 1,
+        "hint": "Hint 1"
+      },
+      {
+        "question": "Question 2",
+        "options": ["option 1", "option 2", "option 3", "option 4"],
+        "answer": 1,
+        "hint": "Hint 2"
+      }
+    ]
+  },
+  
+  {
+    "name": "quiz 2",
+    "created": 1725961545025,
+    "tags": ["tag1", "tag2"],
+    "questions": [
+      {
+        "question": "Question 1",
+        "options": ["option 1", "option 2", "option 3", "option 4"],
+        "answer": 1,
+        "hint": "Hint 1"
+      },
+      {
+        "question": "Question 2",
+        "options": ["option 1", "option 2", "option 3", "option 4"],
+        "answer": 1,
+        "hint": "Hint 2"
+      }
+    ]
+  }
+];
+
+// Expose the data to the global scope
+//window.quizzes = jsFileQuizzesData;
+
+
